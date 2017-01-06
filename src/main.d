95e24a4d9f041d9149d9bddccc4fda683af739a9
ristuo <risto.tuomainen1@gmail.com>
2017-01-06 14:41:55 +0200

    better command line param handling

--- src/main.d
+++ src/main.d
@@ -8,30 +8,48 @@ import filesampler;
 
 int main(string[] args) 
 {
-    string usage = "Usage: " ~ args[0]; 
-    bool tooFewArgs = args.length < 2;
-    string srcPath; 
+    string usage = "Usage: " ~ args[0] ~ " SOURCEFILE SAMPLESIZE [OPTION]"; 
+    if (args.length < 3) 
+    {
+        writeln(usage);
+        return(1);
+    }
+    string srcPath = args[1]; 
+    ulong lines;
+    try
+    {
+        lines = to!ulong(args[2]);
+    }
+    catch (ConvException)
+    {
+        string msg = "Samplesize was \"" ~ args[2];
+        msg ~= "\". It should be integer and in range (0, ";
+        msg ~= text(ulong.max) ~ ")";
+        writeln(msg);
+        return(1);
+    }
+    
     string outPath; 
-    ulong lines = 1;
+    bool verbose = false;
     try
     {
         auto helpInformation = getopt( args
-                                     , "source|s", "Source filepath", &srcPath
-                                     , "lines|n", "Lines to sample", &lines
+                                     , "verbose|v", "Verbose", &verbose
                                      , "out|o", "Output filepath", &outPath );
-        if (tooFewArgs || helpInformation.helpWanted)
+        if (helpInformation.helpWanted)
         {
             defaultGetoptPrinter( usage
                                 , helpInformation.options );
+            return(1);
         }
-        if (outPath == "" || lines < 2)
+        if (outPath == "")
         {
             outPath = stripExtension( baseName( srcPath ) ) ~ "_out.txt";
         }
         debug writeln("srcPath: " ~ srcPath);
         debug writeln("outPath: " ~ outPath);
         debug writeln("lines: " ~ text(lines));
-        sampleOnDisk(srcPath, outPath, lines);
+        sampleOnDisk(srcPath, outPath, lines, verbose);
     }
     catch(GetOptException)
     {
